c4c3142a4de3335cca8c25a84814719466519a1b
petarda33 <sztantics.peter93@gmail.com>
2020-03-04 10:57:33 +0100

    input moving

--- main.js
+++ main.js
@@ -27,3 +27,54 @@ const generatedMap = (sourceMap) => {
 // Height: 13 block
 // Width: 25 block
 // console.log(generatedMap(map));
+
+const stdin = process.stdin;
+stdin.setRawMode(true);
+stdin.resume();
+stdin.setEncoding('utf8');
+playerX = 0;
+playerY = 0;
+
+
+
+const keyProcessor = (key) => {
+  if (key === 'q') {
+    process.exit[0];
+  }
+
+  if (key === 'w' && i > 0) {
+    map[i][j] = ' ';
+    i--;
+    map[i][j] = playerX;
+  }
+  if (key === 's' && i > 0) {
+    map[i][j] = ' ';
+    i++;
+    map[i][j] = playerX;
+  }
+  if (key === 'a' && j < map.length - 1) {
+    map[i][j] = ' ';
+    j++
+    map[i][j] = playerY;
+  }
+  if (key === 'd' && j < map.length) {
+    map[i][j] = ' ';
+    j--;
+    map[i][j] = playerY;
+  }
+};
+stdin.on('data', keyProcessor);
+
+const player = () => {
+  map[i][j] = 'P';
+  return '';
+};
+
+
+const main = () => {
+  player ();
+  setInterval(() => {
+    console.clear();
+    
+  })
+}
--- moving.js
+++ moving.js
@@ -6,22 +6,40 @@ playerX = 0;
 playerY = 0;
 
 const keyProcessor = (key) => {
+  if (key === 'q') {
+    process.exit[0];
+  }
 
   if (key === 'w' && i > 0) {
-    array[i][j] = ' ';
+    map[i][j] = ' ';
     i--;
-    array[i][j] = playerX;
-  } else if (key === 's' && i > 0) {
-    array[i][j] = ' ';
+    map[i][j] = playerX;
+  }
+  if (key === 's' && i > 0) {
+    map[i][j] = ' ';
     i++;
-    array[i][j] = playerX;
-  } else if (key === 'a' && j < array.length) {
-    array[i][j] = ' ';
+    map[i][j] = playerX;
+  }
+  if (key === 'a' && j < map.length - 1) {
+    map[i][j] = ' ';
     j++
-    array[i][j] = playerY;
-  } else if (key === 'd' && j < array.length) {
-    array[i][j] = ' ';
+    map[i][j] = playerY;
+  }
+  if (key === 'd' && j < map.length) {
+    map[i][j] = ' ';
     j--;
-    array[i][j] = playerY;
+    map[i][j] = playerY;
   }
 };
+stdin.on('data', keyProcessor);
+
+const player = () => {
+  map[i][j] = 'P';
+  return '';
+};
+
+
+
+
+
+
