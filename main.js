3dfdca912f1f1228682fd8cfc74c597dfb04a403
JJazon125 <jazon97@gmail.com>
2020-03-04 11:18:41 +0100

    key blinding and mapgen

--- main.js
+++ main.js
@@ -1,5 +1,40 @@
 const common = require('./common.js');
 
+const stdin = process.stdin;
+stdin.setRawMode(true);
+stdin.resume();
+stdin.setEncoding('utf8');
+
+let playerX = 0;
+let playerY = 0;
+const keyProcessor = (key) => {
+  if (key === 'q') {
+    process.exit[0];
+  }
+
+  if (key === 'w' && i > 1) {
+    board[playerX][playerY] = ' ';
+    playerX--;
+    board[playerX][playerY] = player();
+  }
+  if (key === 's' && i < board.length - 1) {
+    board[playerX][playerY] = ' ';
+    playerX++;
+    board[playerX][playerY] = player();
+  }
+  if (key === 'a' && j > 1) {
+    board[playerX][playerY] = ' ';
+    playerY--;
+    board[playerX][playerY] = player();
+  }
+  if (key === 'd' && j < board.length - 1) {
+    board[playerX][playerY] = ' ';
+    playerY++;
+    board[playerX][playerY] = player();
+  }
+};
+stdin.on('data', keyProcessor);
+
 const smallMap = [
   [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
   [2, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2],
