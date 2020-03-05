50fd036d4ecda903524f8039d83d7f70751a10b1
GallDavid <gall1218@yahoo.com>
2020-03-05 14:41:10 +0100

    if player get boosters

--- boosters.js
+++ boosters.js
@@ -1,3 +1,9 @@
+let playerfire = 1;
+let playerbomb = 1;
+let playerlife = 1;
+let player2fire = 1;
+let player2bomb = 1;
+let player2life = 1
 const firepower = 4;
 const plusBomb = 5;
 const pushTheBomb = 6;
@@ -9,7 +15,13 @@ const boosters = (smallMap) => {
     for (let j = 0; j < smallMap[i].length; j++) {
       if (smallMap[i][j] === 1 && chancePercentage < 30) {
         smallMap[i][j] = Math.floor(Math.random() * (7 - 4 + 1)) + 4;
-        console.log(smallMap);
+        if (smallMap[i[j]] === 4) {
+          fire++;
+        } else if (smallMap[i[j]] === 5) {
+          playerbomb++;
+        } else (smallMap[i][j] === 7) {
+          playerlife++;
+        }
       }
     }
   }
