078dadf6a51e41436bebf14c55e1da144c35f3bf
JJazon125 <jazon97@gmail.com>
2020-03-05 21:47:24 +0100

    menu menu menu

--- common.js
+++ common.js
@@ -1,4 +1,7 @@
+const arrays = require('./arrays.js');
 const print = (printable) => {
+  // print an array as string, per line
+  // used everywhere
   console.clear();
   for (let i = 0; i < printable.length; i++) {
     console.log(printable[i].join('').toString());
@@ -6,6 +9,8 @@ const print = (printable) => {
 };
 
 const largeMapGen = (smallMap) => {
+  // make a large map from the small map
+  // used in game
 
   // make a 200x52 array, will be spliced to 176x44
   let board = new Array(52);
@@ -74,6 +79,8 @@ const largeMapGen = (smallMap) => {
 };
 
 const generatedMap = (sourceMap) => {
+  // randomize empty spaces
+  // used in game before generating large map
   for (let i = 0; i < sourceMap.length; i++) {
     for (let j = 0; j < sourceMap[i].length; j++) {
       if (sourceMap[i][j] === 1) {
@@ -89,10 +96,63 @@ const generatedMap = (sourceMap) => {
   return sourceMap;
 };
 
+const menuArrGen = () => {
+  // generates the empty menu array
+  // used in menu
+  let menuArr = new Array(44);
+  for (let i = 0; i < menuArr.length; i++) {
+    menuArr[i] = new Array(176);
+  }
+  return menuArr;
+}
+let menuArr = menuArrGen();
+const menuSelectionMove = () => {
+  // moves the selections arrays
+  // used in menu after menuSelectionDel
+  for (let i = position1, k = 0; k < arrays.arrowLeft.length; i++ , k++) {
+    for (let j = position2, m = 0; m < arrays.arrowLeft[k].length; j++ , m++) {
+      menuArr[i][j] = arrays.arrowLeft[k][m];
+    }
+  }
+  for (let i = position1, k = 0; k < arrays.arrowRight.length; i++ , k++) {
+    for (let j = position2 + 62, m = 0; m < arrays.arrowRight[k].length; j++ , m++) {
+      menuArr[i][j] = arrays.arrowRight[k][m];
+    }
+  }
+};
+let position1 = 13;
+let position2 = 60;
+const menuSelectionDel = () => {
+  // clears the selection arrows
+  // used in menu after moving selection
+  for (let i = position1, k = 0; k < arrays.arrowLeft.length; i++ , k++) {
+    for (let j = position2, m = 0; m < arrays.arrowLeft[k].length; j++ , m++) {
+      menuArr[i][j] = ' ';
+    }
+  }
+  for (let i = position1, k = 0; k < arrays.arrowRight.length; i++ , k++) {
+    for (let j = position2 + 62, m = 0; m < arrays.arrowRight[k].length; j++ , m++) {
+      menuArr[i][j] = ' ';
+    }
+  }
+};
 
+const clearArr = (arr) => {
+  // overwire an array with space
+  // used in menu after option choose
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length; j++) {
+      arr[i][j] = ' ';
+    }
+  }
+};
 
 module.exports = {
   print,
   largeMapGen,
-  generatedMap
+  generatedMap,
+  menuArrGen,
+  menuSelectionMove,
+  menuSelectionDel,
+  clearArr
 };
--- menu.js
+++ menu.js
@@ -1,162 +1,234 @@
 const common = require('./common.js');
+const arrays = require('./arrays.js');
 const stdin = process.stdin;
 stdin.setRawMode(true);
 stdin.resume();
-stdin.setEncoding('utf8');
-console.clear();
+stdin.setEncoding('utf-8');
 const keyProcessor = (key) => {
-  if (key === 'q') {
-    process.exit(0);
+  if (blindset === 0) {
+    if (key === '\u0020') {
+      switch (position1) {
+        case 13:
+          // start game
+          break;
+        case 20:
+          common.print(optionsFunc());
+          break;
+        case 27:
+          // credits
+          break;
+        case 34:
+          process.exit(0);
+          break;
+      }
+    }
+    if (key === 'w' && position1 > 13) {
+      common.menuSelectionDel();
+      position1 -= 7;
+      common.menuSelectionMove();
+      common.print(menuFunc());
+    }
+    if (key === 's' && position1 < 34) {
+      common.menuSelectionDel();
+      position1 += 7;
+      common.menuSelectionMove();
+      common.print(menuFunc());
+    }
   }
-  if (key === 'w' && position1 > 13) {
-    console.clear();
-    del();
-    position1 -= 7;
-    move();
-    common.print(menuArr);
-  }
-  if (key === 's' && position1 < 34) {
-    console.clear();
-    del();
-    position1 += 7;
-    move();
-    common.print(menuArr);
+  if (blindset === 1) {
+    if (key === 'w' && position1 > 13) {
+      common.menuSelectionDel();
+      position1 -= 7;
+      common.menuSelectionMove();
+      common.print(optionsFunc());
+    }
+    if (key === 's' && position1 < 34) {
+      common.menuSelectionDel();
+      position1 += 7;
+      common.menuSelectionMove();
+      common.print(optionsFunc());
+    }
+    if (key === 'a') {
+      switch (position1) {
+        case 13:
+          soundSwitch = true;
+          common.print(optionsFunc());
+          break;
+        case 20:
+          mapSwitch = 0;
+          common.print(optionsFunc());
+          break;
+        case 27:
+          playerSwitch = 0;
+          common.print(optionsFunc());
+          break;
+      }
+    }
+    if (key === 'd') {
+      switch (position1) {
+        case 13:
+          soundSwitch = false;
+          common.print(optionsFunc());
+          break;
+        case 20:
+          mapSwitch = 1;
+          common.print(optionsFunc());
+          break;
+        case 27:
+          playerSwitch = 1;
+          common.print(optionsFunc());
+          break;
+      }
+    }
+
+    if (key === '\u0020' && position1 === 34) {
+      common.print(menuFunc());
+    }
   }
 };
 stdin.on('data', keyProcessor);
-
-let menuArr = new Array(44);
-for (let i = 0; i < menuArr.length; i++) {
-  menuArr[i] = new Array(176);
-}
-
-for (let i = 0; i < menuArr.length; i++) {
-  for (let j = 0; j < menuArr[i].length; j++) {
-    menuArr[i][j] = ' ';
-  }
-}
+let blindset;
 
 let position1 = 13;
 let position2 = 60;
 
-const move = () => {
-  for (let i = position1, k = 0; k < arrowLeft.length; i++ , k++) {
-    for (let j = position2, m = 0; m < arrowLeft[k].length; j++ , m++) {
-      menuArr[i][j] = arrowLeft[k][m];
+let menuArr = common.menuArrGen();
+common.clearArr(menuArr);
+
+const menuFunc = () => {
+  blindset = 0;
+  common.clearArr(menuArr);
+  for (let i = 4, k = 0; k < arrays.bombermanArr.length; i++ , k++) {
+    for (let j = 60, m = 0; m < arrays.bombermanArr[k].length; j++ , m++) {
+      menuArr[i][j] = arrays.bombermanArr[k][m];
     }
   }
-  for (let i = position1, k = 0; k < arrowRight.length; i++ , k++) {
-    for (let j = position2 + 62, m = 0; m < arrowRight[k].length; j++ , m++) {
-      menuArr[i][j] = arrowRight[k][m];
+  for (let i = 13, k = 0; k < arrays.playArr.length; i++ , k++) {
+    for (let j = 82, m = 0; m < arrays.playArr[k].length; j++ , m++) {
+      menuArr[i][j] = arrays.playArr[k][m];
     }
   }
-};
-
-const del = () => {
-  for (let i = position1, k = 0; k < arrowLeft.length; i++ , k++) {
-    for (let j = position2, m = 0; m < arrowLeft[k].length; j++ , m++) {
-      menuArr[i][j] = ' ';
+  for (let i = 20, k = 0; k < arrays.optionsArr.length; i++ , k++) {
+    for (let j = 73, m = 0; m < arrays.optionsArr[k].length; j++ , m++) {
+      menuArr[i][j] = arrays.optionsArr[k][m];
     }
   }
-  for (let i = position1, k = 0; k < arrowRight.length; i++ , k++) {
-    for (let j = position2 + 62, m = 0; m < arrowRight[k].length; j++ , m++) {
-      menuArr[i][j] = ' ';
+  for (let i = 27, k = 0; k < arrays.creditsArr.length; i++ , k++) {
+    for (let j = 74, m = 0; m < arrays.creditsArr[k].length; j++ , m++) {
+      menuArr[i][j] = arrays.creditsArr[k][m];
     }
   }
-};
-
-let bombermanArr = [
-  [' ', ' ', ' ', ' ', '_', '_', '_', '_', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '_', '_', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',],
-  [' ', ' ', ' ', '/', ' ', '_', '_', ' ', ')', ' ', '_', '_', '_', '_', ' ', ' ', ' ', '_', '_', '_', '_', ' ', '_', '_', '_', ' ', ' ', ' ', '/', ' ', '/', '_', ' ', ' ', ' ', '_', '_', '_', ' ', ' ', ' ', '_', '_', '_', '_', '_', ' ', '_', '_', '_', '_', ' ', '_', '_', '_', ' ', ' ', ' ', '_', '_', '_', '_', ' ', '_', ' ', '_', '_', '_', '_',],
-  [' ', ' ', '/', ' ', '_', '_', ' ', ' ', '|', '/', ' ', '_', '_', ' ', '\\', ' ', '/', ' ', '_', '_', ' ', '`', '_', '_', ' ', '\\', ' ', '/', ' ', '_', '_', ' ', '\\', ' ', '/', ' ', '_', ' ', '\\', ' ', '/', ' ', '_', '_', '_', '/', '/', ' ', '_', '_', ' ', '`', '_', '_', ' ', '\\', ' ', '/', ' ', '_', '_', ' ', '`', '/', '/', ' ', '_', '_', ' ', '\\'],
-  [' ', '/', ' ', '/', '_', '/', ' ', '/', '/', ' ', '/', '_', '/', ' ', '/', '/', ' ', '/', ' ', '/', ' ', '/', ' ', '/', ' ', '/', '/', ' ', '/', '_', '/', ' ', '/', '/', ' ', ' ', '_', '_', '/', '/', ' ', '/', ' ', ' ', ' ', '/', ' ', '/', ' ', '/', ' ', '/', ' ', '/', ' ', '/', '/', ' ', '/', '_', '/', ' ', '/', '/', ' ', '/', ' ', '/', ' ', '/'],
-  ['/', '_', '_', '_', '_', '_', '/', ' ', '\\', '_', '_', '_', '_', '/', '/', '_', '/', ' ', '/', '_', '/', ' ', '/', '_', '/', '/', '_', '.', '_', '_', '_', '/', ' ', '\\', '_', '_', '_', '/', '/', '_', '/', ' ', ' ', ' ', '/', '_', '/', ' ', '/', '_', '/', ' ', '/', '_', '/', ' ', '\\', '_', '_', ',', '_', '/', '/', '_', '/', ' ', '/', '_', '/',],
-];
-for (let i = 4, k = 0; k < bombermanArr.length; i++ , k++) {
-  for (let j = 60, m = 0; m < bombermanArr[k].length; j++ , m++) {
-    menuArr[i][j] = bombermanArr[k][m];
+  for (let i = 34, k = 0; k < arrays.exitArr.length; i++ , k++) {
+    for (let j = 81, m = 0; m < arrays.exitArr[k].length; j++ , m++) {
+      menuArr[i][j] = arrays.exitArr[k][m];
+    }
   }
+  for (let i = position1, k = 0; k < arrays.arrowRight.length; i++ , k++) {
+    for (let j = position2 + 62, m = 0; m < arrays.arrowRight[k].length; j++ , m++) {
+      menuArr[i][j] = arrays.arrowRight[k][m];
+    }
+  }
+  for (let i = position1, k = 0; k < arrays.arrowLeft.length; i++ , k++) {
+    for (let j = position2, m = 0; m < arrays.arrowLeft[k].length; j++ , m++) {
+      menuArr[i][j] = arrays.arrowLeft[k][m];
+    }
+  }
+  return menuArr;
+}
+let soundSwitch = true;
+let mapSwitch = 0;
+let playerSwitch = 0;
+const optionsFunc = () => {
+  blindset = 1;
+  console.clear();
+  common.clearArr(menuArr);
+  for (let i = 4, k = 0; k < arrays.bombermanArr.length; i++ , k++) {
+    for (let j = 60, m = 0; m < arrays.bombermanArr[k].length; j++ , m++) {
+      menuArr[i][j] = arrays.bombermanArr[k][m];
+    }
+  }
+  for (let i = 13, k = 0; k < arrays.soundArr.length; i++ , k++) {
+    for (let j = 60, m = 0; m < arrays.soundArr[k].length; j++ , m++) {
+      menuArr[i][j] = arrays.soundArr[k][m];
+    }
+  }
+  switch (soundSwitch) {
+    case true:
+      for (let i = 13, k = 0; k < arrays.onArr.length; i++ , k++) {
+        for (let j = 109, m = 0; m < arrays.onArr[k].length; j++ , m++) {
+          menuArr[i][j] = arrays.onArr[k][m];
+        }
+      }
+      break;
+    case false:
+      for (let i = 13, k = 0; k < arrays.offArr.length; i++ , k++) {
+        for (let j = 109, m = 0; m < arrays.offArr[k].length; j++ , m++) {
+          menuArr[i][j] = arrays.offArr[k][m];
+        }
+      }
+      break;
+  }
+
+  for (let i = 20, k = 0; k < arrays.mapArr.length; i++ , k++) {
+    for (let j = 60, m = 0; m < arrays.mapArr[k].length; j++ , m++) {
+      menuArr[i][j] = arrays.mapArr[k][m];
+    }
+  }
+  switch (mapSwitch) {
+    case 0:
+      for (let i = 20, k = 0; k < arrays.defaultArr.length; i++ , k++) {
+        for (let j = 92, m = 0; m < arrays.defaultArr[k].length; j++ , m++) {
+          menuArr[i][j] = arrays.defaultArr[k][m];
+        }
+      }
+      break;
+    case 1:
+      for (let i = 20, k = 0; k < arrays.randomArr.length; i++ , k++) {
+        for (let j = 88, m = 0; m < arrays.randomArr[k].length; j++ , m++) {
+          menuArr[i][j] = arrays.randomArr[k][m];
+        }
+      }
+      break;
+  }
+
+  for (let i = 27, k = 0; k < arrays.playersArr.length; i++ , k++) {
+    for (let j = 60, m = 0; m < arrays.playersArr[k].length; j++ , m++) {
+      menuArr[i][j] = arrays.playersArr[k][m];
+    }
+  }
+  switch (playerSwitch) {
+    case 0:
+      for (let i = 27, k = 0; k < arrays.twoArr.length; i++ , k++) {
+        for (let j = 111, m = 0; m < arrays.twoArr[k].length; j++ , m++) {
+          menuArr[i][j] = arrays.twoArr[k][m];
+        }
+      }
+      break;
+    case 1:
+      for (let i = 27, k = 0; k < arrays.oneArr.length; i++ , k++) {
+        for (let j = 111, m = 0; m < arrays.oneArr[k].length; j++ , m++) {
+          menuArr[i][j] = arrays.oneArr[k][m];
+        }
+      }
+      break;
+  }
+  for (let i = 34, k = 0; k < arrays.backArr.length; i++ , k++) {
+    for (let j = 81, m = 0; m < arrays.backArr[k].length; j++ , m++) {
+      menuArr[i][j] = arrays.backArr[k][m];
+    }
+  }
+  for (let i = position1, k = 0; k < arrays.arrowRight.length; i++ , k++) {
+    for (let j = position2 + 78, m = 0; m < arrays.arrowRight[k].length; j++ , m++) {
+      menuArr[i][j] = arrays.arrowRight[k][m];
+    }
+  }
+  for (let i = position1, k = 0; k < arrays.arrowLeft.length; i++ , k++) {
+    for (let j = position2 - 14, m = 0; m < arrays.arrowLeft[k].length; j++ , m++) {
+      menuArr[i][j] = arrays.arrowLeft[k][m];
+    }
+  }
+  return menuArr;
 }
 
-let playArr = [
-  [' ', ' ', ' ', ' ', '_', '_', '_', '_', ' ', ' ', ' ', '_', '_', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
-  [' ', ' ', ' ', '/', ' ', '_', '_', ' ', '\\', ' ', '/', ' ', '/', '_', '_', '_', '_', ' ', '_', ' ', '_', '_', ' ', ' ', '_', '_'],
-  [' ', ' ', '/', ' ', '/', '_', '/', ' ', '/', '/', ' ', '/', '/', ' ', '_', '_', ' ', '`', '/', '/', ' ', '/', ' ', '/', ' ', '/'],
-  [' ', '/', ' ', '_', '_', '_', '_', '/', '/', ' ', '/', '/', ' ', '/', '_', '/', ' ', '/', '/', ' ', '/', '_', '/', ' ', '/',],
-  ['/', '_', '/', ' ', ' ', ' ', ' ', '/', '_', '/', ' ', '\\', '_', '_', ',', '_', '/', ' ', '\\', '_', '_', ',', ' ', '/', ' ',],
-  [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '/', '_', '_', '_', '_', '/', ' ', ' ',]
-];
-for (let i = 13, k = 0; k < playArr.length; i++ , k++) {
-  for (let j = 82, m = 0; m < playArr[k].length; j++ , m++) {
-    menuArr[i][j] = playArr[k][m];
-  }
-}
 
-let optionsArr = [
-  [' ', ' ', ' ', '_', '_', '_', '_', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '_', '_', ' ', ' ', ' ', '_', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',],
-  [' ', ' ', '/', ' ', '_', '_', ' ', '\\', ' ', '_', '_', '_', '_', ' ', ' ', ' ', '/', ' ', '/', '_', ' ', '(', '_', ')', '_', '_', '_', '_', ' ', ' ', ' ', '_', '_', '_', '_', ' ', ' ', ' ', '_', '_', '_', '_', '_'],
-  [' ', '/', ' ', '/', ' ', '/', ' ', '/', '/', ' ', '_', '_', ' ', '\\', ' ', '/', ' ', '_', '_', '/', '/', ' ', '/', '/', ' ', '_', '_', ' ', '\\', ' ', '/', ' ', '_', '_', ' ', '\\', ' ', '/', ' ', '_', '_', '_', '/'],
-  ['/', ' ', '/', '_', '/', ' ', '/', '/', ' ', '/', '_', '/', ' ', '/', '/', ' ', '/', '_', ' ', '/', ' ', '/', '/', ' ', '/', '_', '/', ' ', '/', '/', ' ', '/', ' ', '/', ' ', '/', '(', '_', '_', ' ', ' ', ')',],
-  ['\\', '_', '_', '_', '_', '/', '/', ' ', '.', '_', '_', '_', '/', ' ', '\\', '_', '_', '/', '/', '_', '/', ' ', '\\', '_', '_', '_', '_', '/', '/', '_', '/', ' ', '/', '_', '/', '/', '_', '_', '_', '_', '/', ' ',],
-  [' ', ' ', ' ', ' ', ' ', '/', '_', '/', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',]
-];
-for (let i = 20, k = 0; k < optionsArr.length; i++ , k++) {
-  for (let j = 73, m = 0; m < optionsArr[k].length; j++ , m++) {
-    menuArr[i][j] = optionsArr[k][m];
-  }
-}
-
-let creditsArr = [
-  [' ', ' ', ' ', '_', '_', '_', '_', '_', '_', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '_', '_', ' ', '_', ' ', ' ', '_', '_', ' ', ' ', ' ', ' ', ' ', ' ',],
-  [' ', ' ', '/', ' ', '_', '_', '_', '_', '/', '_', '_', '_', '_', '_', ' ', '_', '_', '_', ' ', ' ', ' ', '_', '_', '_', '_', '/', ' ', '/', '(', '_', ')', '/', ' ', '/', '_', ' ', '_', '_', '_', '_', '_'],
-  [' ', '/', ' ', '/', ' ', ' ', ' ', ' ', '/', ' ', '_', '_', '_', '/', '/', ' ', '_', ' ', '\\', ' ', '/', ' ', '_', '_', ' ', ' ', '/', '/', ' ', '/', '/', ' ', '_', '_', '/', '/', ' ', '_', '_', '_', '/'],
-  ['/', ' ', '/', '_', '_', '_', ' ', '/', ' ', '/', ' ', ' ', ' ', '/', ' ', ' ', '_', '_', '/', '/', ' ', '/', '_', '/', ' ', '/', '/', ' ', '/', '/', ' ', '/', '_', ' ', '(', '_', '_', ' ', ' ', ')',],
-  ['\\', '_', '_', '_', '_', '/', '/', '_', '/', ' ', ' ', ' ', ' ', '\\', '_', '_', '_', '/', ' ', '\\', '_', '_', ',', '_', '/', '/', '_', '/', ' ', '\\', '_', '_', '/', '/', '_', '_', '_', '_', '/', ' ',]
-];
-for (let i = 27, k = 0; k < creditsArr.length; i++ , k++) {
-  for (let j = 74, m = 0; m < creditsArr[k].length; j++ , m++) {
-    menuArr[i][j] = creditsArr[k][m];
-  }
-}
-
-let exitArr = [
-  [' ', ' ', ' ', ' ', '_', '_', '_', '_', '_', '_', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '_', ' ', ' ', '_', '_',],
-  [' ', ' ', ' ', '/', ' ', '_', '_', '_', '_', '/', '_', ' ', ' ', '_', '_', ' ', '(', '_', ')', '/', ' ', '/', '_'],
-  [' ', ' ', '/', ' ', '_', '_', '/', ' ', ' ', '|', ' ', '|', '/', '_', '/', '/', ' ', '/', '/', ' ', '_', '_', '/'],
-  [' ', '/', ' ', '/', '_', '_', '_', ' ', '_', '>', ' ', ' ', '<', ' ', '/', ' ', '/', '/', ' ', '/', '_', ' ',],
-  ['/', '_', '_', '_', '_', '_', '/', '/', '_', '/', '|', '_', '|', '/', '_', '/', ' ', '\\', '_', '_', '/', ' ',]
-];
-for (let i = 34, k = 0; k < exitArr.length; i++ , k++) {
-  for (let j = 81, m = 0; m < exitArr[k].length; j++ , m++) {
-    menuArr[i][j] = exitArr[k][m];
-  }
-}
-
-
-let arrowRight = [
-  [' ', ' ', '_', '_', ' ', '_', '_'],
-  [' ', '/', ' ', '/', '/', ' ', '/'],
-  ['/', ' ', '/', '/', ' ', '/',],
-  ['\\', ' ', '\\', '\\', ' ', '\\',],
-  [' ', '\\', '_', '\\', '\\', '_', '\\']
-];
-for (let i = 13, k = 0; k < arrowRight.length; i++ , k++) {
-  for (let j = 122, m = 0; m < arrowRight[k].length; j++ , m++) {
-    menuArr[i][j] = arrowRight[k][m];
-  }
-}
-
-let arrowLeft = [
-  ['_', '_', ' ', '_', '_', ' ',],
-  ['\\', ' ', '\\', '\\', ' ', '\\',],
-  [' ', '\\', ' ', '\\', '\\', ' ', '\\'],
-  [' ', '/', ' ', '/', '/', ' ', '/'],
-  ['/', '_', '/', '/', '_', '/',]
-];
-for (let i = 13, k = 0; k < arrowLeft.length; i++ , k++) {
-  for (let j = 60, m = 0; m < arrowLeft[k].length; j++ , m++) {
-    menuArr[i][j] = arrowLeft[k][m];
-  }
-}
-
-common.print(menuArr);
+common.print(menuFunc());
